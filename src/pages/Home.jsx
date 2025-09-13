7244f0c5bd8d625e1843ba86dcf8383a5d8b7279
Wesoft90 <shivam.u@wesoftek.com>
2025-09-13 19:45:58 +0530

    Newly Code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Hero2 from './Hero2'
+import Hero1 from './Hero1'
 
 const Home = () => {
   return (
     <div>
+      <Hero1 />
       <Hero2 />
     </div>
   )
